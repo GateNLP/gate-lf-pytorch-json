98bf84dccbafdb387a9a8c7902b02158e5d45f11
Johann Petrak <johann.petrak@gmail.com>
2018-02-10 16:14:49 +0000

    WIP

--- gatelfpytorch/classificationmodelsimple.py
+++ gatelfpytorch/classificationmodelsimple.py
@@ -1,10 +1,60 @@
-from . modelbuilder import ModelBuilder
+import torch.nn
+from torch.autograd import Variable as V
+import torch.nn.functional as F
 
-class ClassificationModelSimple(ModelBuilder):
 
-    def __init__(self, dataset):
-        super().__init__(dataset)
-        self.dataset = dataset
+class ClassificationModelSimple(torch.nn.Module):
 
-    def train(self):
-        pass
+    def __init__(self, inputlayersinfo, hiddenlayersinfo, outputlayerinfo,
+                 featureconfig):
+        """Construct the model: inputlayers is a list of tuples, where
+        the first element is the PyTorch module and the second element
+        is a dictionary of config data for that layer. The config data
+        must contain the "type" key. Hiddenlayers is a  similar list
+        of hidden layers, where the first element of the list corresponds
+        to the layer after the input layer and the last element to
+        the layer before the output layer.
+        Output layer is a single tuple (module,configs).
+        The featureconfig parameter contains the feature indices
+        per type and maybe other config info created by the wrapper.
+        """
+        super().__init__()
+        self.inputlayersinfo = inputlayersinfo
+        self.hiddenlayersinfo = hiddenlayersinfo
+        self.outputlayerinfo = outputlayerinfo
+        self.num_idxs = featureconfig["num_idxs"]
+        self.nom_idxs = featureconfig["nom_idxs"]
+        self.ngr_idxs = featureconfig["ngr_idxs"]
+
+    def forward(self, batch):
+        i_nom = 0
+        i_ngr = 0
+        input_layer_outputs = []
+        for inputlayer, config in self.inputlayersinfo:
+            inputtype = config["type"]
+            if inputtype == "numeric":
+                vals = [batch[i] for i in self.num_idxs]
+                vals4pt = V(torch.FloatTensor(vals).t(), requires_grad=True)
+                out = inputlayer(vals4pt)
+                input_layer_outputs.append(out)
+            elif inputtype == "nominal":
+                nom_idx = self.nom_idxs[i_nom]
+                i_nom += 1
+                vals4pt = self.convert_nominal(batch[nom_idx])
+                out = inputlayer(vals4pt)
+                input_layer_outputs.append(out)
+            elif inputtype == "ngram":
+                ngr_idx = self.ngr_idxs[i_ngr]
+                i_ngr += 1
+                vals4pt = self.convert_ngram(batch[ngr_idx])
+                out = inputlayer(vals4pt)
+                input_layer_outputs.append(out)
+            else:
+                raise Exception("Odd input type: %s" % inputtype)
+            # concatenate the outputs
+            hidden_vals = torch.cat(input_layer_outputs)
+            for hiddenlayer, config in self.hiddenlayersinfo:
+                hidden_vals = hiddenlayer(hidden_vals)
+            outputlayer, outputlayerconfig = self.outputlayerinfo
+            out = outputlayer(hidden_vals)
+            return out
--- gatelfpytorch/modelwrapper.py
+++ gatelfpytorch/modelwrapper.py
@@ -12,12 +12,18 @@ class ModelWrapper(object):
 
     # This requires an initialized dataset instance
     def __init__(self, dataset):
-        pass
+        self.num_idxs = dataset.get_numeric_feature_idxs()
+        self.nom.idxs = dataset.get_nominal_feature_idxs()
+        self.ngr.idxs = dataset.get_ngram_feature_idxs()
+        num_feats = dataset.get_numeric_features()
+        nom_feats = dataset.get_nominal_features()
+        ngr_feats = dataset.get_ngram_features()
+
 
     # the implementation should figure out best values if parameter
     # is set to None
     # Also, by default, the method should decide which format
     # to use for reading the data (original or converted)
     def train(self, epochs=None, batchsize=None):
-        pass
-
+        # for each input, pass it the corresponding feature or features
+        # then concatenate all the outputs to form the first hidden layer

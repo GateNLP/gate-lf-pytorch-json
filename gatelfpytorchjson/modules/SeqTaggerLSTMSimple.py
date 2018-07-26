603f155fc9c4f82a854ab225647441ba5b99dbef
Johann Petrak <johann.petrak@gmail.com>
2018-07-26 15:03:35 +0100

    WIP

--- gatelfpytorchjson/modules/SeqTaggerLSTMSimple.py
+++ gatelfpytorchjson/modules/SeqTaggerLSTMSimple.py
@@ -15,54 +15,45 @@ class SeqTaggerLSTMSimple(CustomModule):
         """
         # first get the parameters so we can create the same type of vector, from the same device easily
         # the lstm is really wrapped in a a "takefromtuple" layer, so we have to get it out of there
-        lstmlayer = dict(self.layers.named_modules())['lstm'].module
-        lstmparms = next(lstmlayer.parameters()).data
-        h1 = V(lstmparms.new(lstmlayer.num_layers, batchsize, lstmlayer.hidden_size).zero_())
-        h2 = V(lstmparms.new(lstmlayer.num_layers, batchsize, lstmlayer.hidden_size).zero_())
-        return (h1, h2)
-
+        lstmparms = next(self.layer_lstm.parameters()).data
+        h0 = lstmparms.new(self.layer_lstm.num_layers, batchsize, self.layer_lstm.hidden_size)
+        c0 = lstmparms.new(self.layer_lstm.num_layers, batchsize, self.layer_lstm.hidden_size)
+        # default is to set everything to zero
+        h0.zero_()
+        c0.zero_()
+        # or we set to small uniformly distributed random numbers
+        #h0.copy_(torch.rand(self.layer_lstm.num_layers, batchsize, self.layer_lstm.hidden_size)*0.01)
+        #c0.copy_(torch.rand(self.layer_lstm.num_layers, batchsize, self.layer_lstm.hidden_size)*0.01)
+        return h0, c0
 
     # NOTE: make sure the dataset is not stored and only used to decide on parameters etc so
     # that the dataset data is not getting pickled when the model is saved!
     def __init__(self, dataset, config={}):
         super().__init__(config=config)
 
-        n_classes = dataset.get_info()["nClasses"]
+        self.n_classes = dataset.get_info()["nClasses"]
         # create a simple LSTM-based network: the input uses an embedding layer created from
         # the vocabulary, then a bidirectional LSTM followed by a simple softmax layer for each element
         # in the sequence
         feature = dataset.get_index_features()[0]
         vocab = feature.vocab
         # create the embedding layer from the vocab
-        emblayer = EmbeddingsModule(vocab)
+        self.layer_emb = EmbeddingsModule(vocab)
 
-        # TODO!!!! To allow initialisation of the hidden state etc. we need to split up
-        # the layers into at least input/lstm/output or just keep them all separately!
-        # Forget sequence!
-
-        lstm_hiddenunits = 100
-        lstm_nlayers = 1
-        is_bidirectional = False
-        lstmlayer = torch.nn.LSTM(input_size=emblayer.emb_dims,
-                                hidden_size=lstm_hiddenunits,
-                                num_layers=lstm_nlayers,
-                                # dropout=0.5,
-                                bidirectional=is_bidirectional,
-                                batch_first=True)
-        lstmlayer = TakeFromTuple(lstmlayer, which=0)
-        lin_units = lstm_hiddenunits*2 if is_bidirectional else lstm_hiddenunits
+        self.lstm_hiddenunits = 100
+        self.lstm_nlayers = 1
+        self.lstm_is_bidirectional = False
+        self.layer_lstm = torch.nn.LSTM(input_size=self.layer_emb.emb_dims,
+                                        hidden_size=self.lstm_hiddenunits,
+                                        num_layers=self.lstm_nlayers,
+                                        dropout=0.5,
+                                        bidirectional=self.lstm_is_bidirectional,
+                                        batch_first=True)
+        lin_units = self.lstm_hiddenunits*2 if self.lstm_is_bidirectional else self.lstm_hiddenunits
         self.lstm_totalunits = lin_units
-        self.lstm_nlayers = lstm_nlayers
-        linlayer = torch.nn.Linear(lin_units, n_classes)
-        outlayer = torch.nn.LogSoftmax(dim=1)
-        layer_seq = OrderedDict()
-        layer_seq['embs'] = emblayer
-        layer_seq['lstm'] = lstmlayer
-        layer_seq['lin'] = linlayer
-        layer_seq['out'] = outlayer
-        self.layers = torch.nn.Sequential(layer_seq)
-        print("Network: \n", self.layers, file=sys.stderr)
-        self.get_init_weights(1)
+        self.layer_lin = torch.nn.Linear(lin_units, self.n_classes)
+        self.layer_out = torch.nn.LogSoftmax(dim=1)
+        print("Network: \n", self, file=sys.stderr)
 
     # this gets a batch if independent variables
     # By default this is in reshaped padded batch format.
@@ -76,11 +67,21 @@ class SeqTaggerLSTMSimple(CustomModule):
     # HOWEVER: we have decided not to put the indices on cuda
     def forward(self, batch):
         # we need only the first feature:
+        # print("DEBUG: batch=", batch, file=sys.stderr)
         batch = torch.LongTensor(batch[0])
+        # print("DEBUG: batch size=", batch.size(), file=sys.stderr)
         if self.on_cuda():
             batch.cuda()
-        out, hidden = self.layers(batch)
-        return out, hidden
+        tmp = self.layer_emb(batch)
+        # print("DEBUG: embout=", tmp, file=sys.stderr)
+        hidden_init = self.get_init_weights(len(batch))
+        # print("DEBUG: lstm input size=", tmp.size(), file=sys.stderr)
+        # print("DEBUG: hidden_init[0] size=", hidden_init[0].size(), file=sys.stderr)
+        # print("DEBUG: hidden_init[1] size=", hidden_init[1].size(), file=sys.stderr)
+        tmp, hidden = self.layer_lstm(tmp, hidden_init)
+        tmp = self.layer_lin(tmp)
+        out = self.layer_out(tmp)
+        return out
 
     def get_lossfunction(self, config={}):
         # IMPORTANT: for the target indices, we use -1 for padding by default!

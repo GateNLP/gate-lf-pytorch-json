6cec1a071d9835a5f1098e208719b3cedf0eae95
Johann Petrak <johann.petrak@gmail.com>
2018-08-01 15:37:06 +0100

    Add python docs.

--- gendoc.sh
+++ gendoc.sh
@@ -8,7 +8,7 @@ rm -rf $HTMDIR
 mkdir $SPHDIR
 mkdir $HTMDIR
 # cp sphinx-config/index.rst doc-sphinx
-sphinx-apidoc -e -f -H "GATE LF Pytorch Wrapper (gatelfpytorch)" -A "Johann Petrak" -V "0.1" --ext-autodoc --ext-githubpages -o $SPHDIR gatelfpytorch
+sphinx-apidoc -e -f -V "0.1" --ext-autodoc --ext-githubpages -o $SPHDIR gatelfpytorchjson
 mv $SPHDIR/modules.rst $SPHDIR/index.rst
 sphinx-build -b html -c sphinx-config $SPHDIR $HTMDIR
 cp -r $HTMDIR/* docs/pythondoc/
--- sphinx-config/conf.py
+++ sphinx-config/conf.py
@@ -5,8 +5,13 @@ project = 'GATE LF Pytorch Wrapper (gatelfpytorch)'
 copyright = '2018, University of Sheffield'
 author = 'Johann Petrak'
 
-version = '0.1'
+# version = '0.1'
 
+html_theme = "classic"
+html_theme_options = {
+    "rightsidebar": "false",
+    "relbarbgcolor": "black"
+}
 
 extensions = [
     'sphinx.ext.autodoc',

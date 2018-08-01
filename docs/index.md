2128db5f3208b53422cc2477b5fb4b25ecebc014
Johann Petrak <johann.petrak@gmail.com>
2018-08-01 15:27:34 +0100

    Prepare for putting the documentation on github pages.

---
+++ docs/index.md
@@ -0,0 +1,16 @@
+# gate-lf-pytorch-json - A Library for using PyTorch with the GATE LearningFramework
+
+This is a python library that allows to train and use models created with PyTorch for
+the GATE LearningFramework plugin. 
+
+## Requirements
+
+Requirements:
+* Python 3.5 or later - this does not work with python 2.x!!
+* Python package `numpy`
+* Python package `torch`  (PyTorch) 
+
+## Overview of the documentation:
+
+* [Usage Examples](UsageExamples)
+* [The Generated Python Documentation](pythondoc)
--- gendoc.sh
+++ gendoc.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-export PYTHONPATH=.:../gate-lf-python-data
-rm -rf ./doc-sphinx
-rm -rf ./doc-html
-mkdir doc-sphinx
-mkdir doc-html
+export PYTHONPATH=`pwd`:../gate-lf-python-data
+SPHDIR=./tmp-doc-sphinx
+HTMDIR=./tmp-doc-html
+rm -rf $SPHDIR
+rm -rf $HTMDIR
+mkdir $SPHDIR
+mkdir $HTMDIR
 # cp sphinx-config/index.rst doc-sphinx
-sphinx-apidoc -e -f -H "GATE LF Pytorch Wrapper (gatelfpytorch)" -A "Johann Petrak" -V "0.1" -o doc-sphinx gatelfpytorch
-mv doc-sphinx/modules.rst doc-sphinx/index.rst
-sphinx-build -b html -c sphinx-config doc-sphinx doc-html 
+sphinx-apidoc -e -f -H "GATE LF Pytorch Wrapper (gatelfpytorch)" -A "Johann Petrak" -V "0.1" --ext-autodoc --ext-githubpages -o $SPHDIR gatelfpytorch
+mv $SPHDIR/modules.rst $SPHDIR/index.rst
+sphinx-build -b html -c sphinx-config $SPHDIR $HTMDIR
+cp -r $HTMDIR/* docs/pythondoc/
